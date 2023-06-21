de0184f5595373c2b61379bedee0f49a1e882cbc
Raghu K <raghuk.vit@gmail.com>
2023-06-21 08:26:58 +0530

    Update roboshop-terraform/

--- env-dev/main.tfvars
+++ env-dev/main.tfvars
@@ -46,6 +46,8 @@ tags = {
 
 vpc = {
   main = {
-    cidr_block = "10.0.0.0/16"
+    cidr_block            = "10.0.0.0/16"
+    web_subnet_cidr_block = ["10.0.0.0/24"]
   }
 }
+
--- main.tf
+++ main.tf
@@ -1,7 +1,11 @@
 module "vpc" {
   source = "git::https://github.com/raghudevopsb73/tf-module-vpc.git"
 
-  for_each   = var.vpc
-  cidr_block = each.value["cidr_block"]
+  for_each              = var.vpc
+  cidr_block            = each.value["cidr_block"]
+  web_subnet_cidr_block = each.value["web_subnet_cidr_block"]
+
+  env  = var.env
+  tags = var.tags
 
 }
