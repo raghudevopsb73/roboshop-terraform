2c92ecf49139ab8c04219f5a387b476a06b71628
Raghu K <raghuk.vit@gmail.com>
2023-06-23 08:08:42 +0530

    Update roboshop-terraform/

--- main.tf
+++ main.tf
@@ -33,6 +33,7 @@ module "rabbitmq" {
 
   sg_subnet_cidr = lookup(lookup(lookup(lookup(var.vpc, "main", null), "subnets", null), "app", null), "cidr_block", null)
   vpc_id         = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
+  subnet_id      = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnet_ids", null), "db", null), "subnet_ids", null)[0]
 
   env  = var.env
   tags = var.tags
