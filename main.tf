bea238737acdacd6a865156781818523db6fd7e5
Raghu K <raghuk.vit@gmail.com>
2023-06-27 08:44:43 +0530

    Update roboshop-terraform/

--- main.tf
+++ main.tf
@@ -106,7 +106,7 @@ module "alb" {
   for_each           = var.alb
   name               = each.value["name"]
   internal           = each.value["internal"]
-  load_balancer_type = each.type["load_balancer_type"]
+  load_balancer_type = each.value["load_balancer_type"]
   vpc_id             = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
   sg_subnet_cidr     = each.value[name] == "public" ? ["0.0.0.0/0"] : local.app_web_subnet_cidr
   subnets            = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnet_ids", null), each.value["subnet_ref"], null), "subnet_ids", null)
