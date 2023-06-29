49c1a26fa120c863a5a3a4c35d40f9273589272c
Raghu K <raghuk.vit@gmail.com>
2023-06-29 08:21:27 +0530

    Update roboshop-terraform/

--- main.tf
+++ main.tf
@@ -27,7 +27,7 @@ module "rabbitmq" {
   tags           = var.tags
   allow_ssh_cidr = var.allow_ssh_cidr
   zone_id        = var.zone_id
-  kms_key_id     = var.kms_key_id
+  kms_key_arn    = var.kms_key_arn
 }
 
 module "rds" {
